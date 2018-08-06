762e67f46c143d1272d8f1f22f99f143c6bb1668
Tibo Keri <tiborkeri@icloud.com>
2018-08-06 21:42:38 +0100

    Create initial state and pass as props to App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import state from './state'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App {...state} />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/state.js
@@ -0,0 +1,8 @@
+const state = {
+  targetValue: 999,
+  currentValue: 500,
+  endDate: '12/12/2018',
+  supporters: 42
+}
+
+export default state
